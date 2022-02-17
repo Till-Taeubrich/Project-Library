81c814b550ffaa96ebda580b88046c90eb8668b9
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-02-18 10:49:52 +1100

    Step 3 done with bug.
     Bug: creates an extra row with each Input.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Data
-let myLibrary = [];
+let library = [];
 
-class book {
+class books {
   constructor(title, author, numberOfPages, readingStatus) {
     this.title = title;
     this.author = author;
@@ -10,20 +10,80 @@ class book {
   }
 }
 
-function addBookToLibrary() {
-  let title = prompt('title','');
-  let author = prompt('author','');
-  let numberOfPages = prompt('numberOfPages','');
+function getUserInput(){
+  const title =  prompt('title','');
+  const author = prompt('author','');
+  let numberOfPages = Number(prompt('numberOfPages', ''));
   let readingStatus = prompt('readingStatus','');
-
-  let newBook = new book(title, author, numberOfPages, readingStatus);
   
-  myLibrary.push(newBook);
+  addBookToLibrary(title, author, numberOfPages, readingStatus);
 }
+
+function addBookToLibrary(title, author, numberOfPages, readingStatus) {
+  let newBook = new books(title, author, numberOfPages, readingStatus);
+  library.push(newBook);
+  displayLibrary(title, author, numberOfPages, readingStatus);
+}
+
+
+
+
+function displayLibrary(title, author, numberOfPages, readingStatus){
+
+  const tableBody = document.querySelector('.tableBody');
+
+  for (const book of library) {
+
+    const newRow = document.createElement('tr');
+    tableBody.append(newRow);
+
+    const titleColumn = document.createElement('td');
+    // titleColumn.classList.add('title');
+
+    const authorColumn = document.createElement('td');
+    // authorColumn.classList.add('author');
+
+    const pagesColumn = document.createElement('td');
+    // pagesColumn.classList.add('pages');
+
+    const statusColumn = document.createElement('td');
+    // statusColumn.classList.add('status');
+
+
+    
+    titleColumn.textContent = title;
+    tableBody.append(titleColumn);
+
+    authorColumn.textContent = author;
+    tableBody.append(authorColumn);
+
+    pagesColumn.textContent = numberOfPages;
+    tableBody.append(pagesColumn);
+
+    statusColumn.textContent = readingStatus;
+    tableBody.append(statusColumn);
+    
+
+
+
+    // const titleColumn = document.createElement('td');
+    // const authorColumn = document.createElement('td');
+    // const pagesColumn = document.createElement('td');
+    // const statusColumn = document.createElement('td');
+  }
+}
+
+
+
+
+
 
 // UI
 const button = document.querySelector('.addBook');
+const titleField = document.querySelector('.title')
+const authorField = document.querySelector('.author')
+const pagesField = document.querySelector('.pages')
+const statusField = document.querySelector('.status')
 
 
-
-button.addEventListener('click', addBookToLibrary);
+button.addEventListener('click', getUserInput);
